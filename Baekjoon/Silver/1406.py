ff96eda77aa136daa50487a3add6b3ba1f666990
imjaehyeong <dlawogud517@gmail.com>
2023-12-27 12:49:17 +0900

    solved

--- Baekjoon/Silver/1406.py
+++ Baekjoon/Silver/1406.py
@@ -1,9 +1,21 @@
-string = input()
-commandCount = int(input())
+import sys
+leftStack = list(sys.stdin.readline().rstrip())
+rightstack = []
+commandCount = int(sys.stdin.readline().rstrip())
+for i in range(commandCount):
+    command = sys.stdin.readline().split()
+    if command[0] == 'L':
+        if leftStack:
+            rightstack.append(leftStack.pop())
+        
+    elif command[0] == 'D':
+        if rightstack:
+            leftStack.append(rightstack.pop())
+    elif command[0] == 'B':
+        if leftStack:
+            leftStack.pop()
 
-command = input()
-string = input()
+    elif command[0] == 'P':
+        leftStack.append(command[1])
 
-
-
-
+print(''.join(map(str, leftStack)), ''.join(map(str, reversed(rightstack))),sep="")
