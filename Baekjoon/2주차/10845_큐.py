08da4c42bd76924c212d3ce05e8c323785c48fbe
imjaehyeong <dlawogud517@gmail.com>
2024-01-03 12:10:23 +0900

    10845

---
+++ Baekjoon/2주차/10845_큐.py
@@ -0,0 +1,35 @@
+from collections import deque
+
+n = int(input())
+queue = deque()
+printList = []
+for i in range(n):
+    command = input().split()
+    if command[0] == "push":
+        queue.append(command[1])
+    elif command[0] == "pop":
+        if len(queue) == 0:
+            printList.append(-1)
+        else:
+            printList.append(queue.popleft())
+    elif command[0] == "size":
+        printList.append((len(queue)))
+    elif command[0] == "empty":
+        if len(queue) == 0:
+            printList.append(1)
+        else:
+            printList.append(0)
+    elif command[0] == "front":
+        if len(queue) == 0:
+            printList.append(-1)
+        else:
+            printList.append(queue[0])
+    elif command[0] == "back":
+        if len(queue) == 0:
+            printList.append(-1)
+        else:
+            printList.append(queue[-1])
+    else:
+        print("error")
+for i in range(len(printList)):
+    print(printList[i])
