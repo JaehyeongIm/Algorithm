9bb9eb19bd996a49a6a7bacfad5caae7269c19b9
imjaehyeong <dlawogud517@gmail.com>
2024-01-03 15:43:44 +0900

    solved

---
+++ Baekjoon/2주차/2164_카드2.py
@@ -0,0 +1,16 @@
+import sys
+from collections import deque
+n = int(sys.stdin.readline().rstrip())
+
+dQ = deque(range(1,n+1))
+count=0
+while len(dQ)>1: # n-1 O(n)
+    count+=1
+    if count%2 == 1:
+        dQ.popleft()
+    else:
+        dQ.append(dQ.popleft())
+
+
+if len(dQ)==1:
+    print(dQ[-1])
--- Baekjoon/2주차/4949_균형잡힌세상.py
+++ Baekjoon/2주차/4949_균형잡힌세상.py
@@ -1,2 +1,36 @@
-s = input()
+import sys
 
+
+output = []
+while True: # 입력 수:n 문자열 길이:m O(nm)
+    stack = []
+    input = sys.stdin.readline().rstrip()
+    if input == ".":
+        break
+    inputList = list(input)
+    # 처음 기호로 )이나 ]을 만나는 경우 flag 을 true로 설정하여 output에 no로 설정함.
+    flag=True
+
+    for char in inputList:
+        if char == "(" or char == "[":
+            stack.append(char)
+        elif char == ")":
+            if len(stack)!=0 and "(" == stack[-1]:
+                stack.pop()
+            else:
+                flag=False
+                break
+        elif char == "]":
+            if len(stack)!=0 and "[" == stack[-1]:
+                stack.pop()
+            else:
+                flag=False
+                break
+
+    if len(stack) == 0 and flag: # 바로 )이나 ]을 만나는 경우 스택은 비어있는데 no이기 때문에 flag 사용
+        output.append("yes")
+    else:
+        output.append("no")
+
+for i in output:
+    print(i)
