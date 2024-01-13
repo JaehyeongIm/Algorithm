247c420ed0f1e430deb8279223aebbaa1b55972f
imjaehyeong <dlawogud517@gmail.com>
2024-01-14 00:07:16 +0900

    two prolbem solved

--- Baekjoon/4주차/1012_유기농배추.py
+++ Baekjoon/4주차/1012_유기농배추.py
@@ -1,15 +1,39 @@
+import sys
+sys.setrecursionlimit(10**5)
+# 파이썬 재귀는 기본적으로 1000임 (재귀 반복으로 인한 stack overflow 막기위해서) 이 코드로 늘려줄 수 있음
+
+
+def dfs(x,y):
+    if x<0 or x>=M or y<0 or y>=N:
+        return False
+    if graph[x][y]== 1:
+        #방문처리
+        graph[x][y] = 0
+        dfs(x+1,y)
+        dfs(x-1,y)
+        dfs(x,y+1)
+        dfs(x,y-1)
+        return True
+    return False
+
 T = int(input())
+resultList=[]
 for i in range(T):
     graph = []
     M,N,K = map(int,input().split())
-    for _ in range(N):
+    result=0
+    # 그래프 0으로 세팅
+    for _ in range(M):
         graph.append([0] * N)
-    for _ in range(int(K)):
+
+    # 그래프 입력좌표 1로 세팅
+    for _ in range(K):
         x, y = map(int,input().split())
         graph[x][y]=1
-    for
-
-
-
-def bfs(V):
-    list=[]
+    for i in range(M):
+        for j in range(N):
+            if dfs(i,j)==True:
+                result+=1
+    resultList.append(result)
+for i in resultList:
+    print(i)
---
+++ Baekjoon/4주차/1697_숨바꼭질.py
@@ -0,0 +1,24 @@
+from collections import deque
+N,K = map(int,input().split())
+visited=[0]*1000001
+
+
+def bfs(a, b):
+    queue = deque()
+    queue.append((a,0))
+    visited[a]=1
+    while queue:
+        V,steps=queue.popleft()
+        if V == b:
+            return steps
+        for nextNode in [V-1,V+1,2*V]:
+            if 0<=nextNode<100001 and  not visited[nextNode] :
+                queue.append((nextNode,steps+1))
+                visited[nextNode]=1
+    return -1
+result = bfs(N,K)
+print(result)
+
+# a라는 값의 노드는 인접한 a-1, a+1 , 2a의 노드가 있고 이 구조가 재귀적으로 반복된다.
+# 깊이정보를 튜플로 가지고 탐색하며, 방문목록을 list로 따로 두어, 방문하지 않은 노드만 방문한다.
+# bfs로 b의 값이 될때까지 탐색한다. 도착한 순간 바로 그 깊이정보가 최단거리 값이 된다.
