0ac046f9d18fee2bbd71254b7e2f323e6a62b6ee
imjaehyeong <dlawogud517@gmail.com>
2024-02-25 19:51:11 +0900

    9662 N-Queen 해결

---
+++ Baekjoon/9주차/9663_N-queen.py
@@ -0,0 +1,32 @@
+
+N = int(input())
+cnt=0
+leftDiagonal=[0] * 1000000
+straight=[0] * 1000000
+rightDiagonal=[0]* 1000000
+
+
+# (row,col)에 둘수 있는지 확인하는 함수
+def recursion(row):
+    global cnt
+
+    if row == N:
+        cnt+=1
+        return
+    for col in range(N):
+        # 퀸 이동 반경에 걸리면 contiunue
+        if leftDiagonal[row+col] or straight[col] or rightDiagonal[row-col+N-1]:
+            continue
+        straight[col]=1
+        leftDiagonal[row+col]=1
+        rightDiagonal[row-col+N-1] = 1
+        recursion(row+1)
+        straight[col]=0
+        leftDiagonal[row+col]=0
+        rightDiagonal[row-col+N-1] = 0
+
+
+recursion(0)
+print(cnt)
+
+
