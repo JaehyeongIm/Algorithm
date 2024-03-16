c5f0a443778a5f25f5a78c149a61f3e2c3a02696
imjaehyeong <dlawogud517@gmail.com>
2024-03-16 21:48:40 +0900

    1463_1로만들기

---
+++ Baekjoon/11주차/1149_RGB거리.py
@@ -0,0 +1,18 @@
+N = int(input())
+cost= []
+for i in range(N):
+    cost.append(list(map(int,input().split())))
+
+# 0 - 빨강 1 - 녹색 2 - 파랑
+# 초기 설정
+D = [[0,0,0] for _ in range(N)]
+D[0][0] = cost[0][0]
+D[0][1] = cost[0][1]
+D[0][2] = cost[0][2]
+
+# D[i] = i번째를 칠했을때 비용의 최솟값 0,1,2 = 빨강,녹색,파랑
+for i in range(1, N):
+    D[i][0] = min(D[i-1][2]+cost[i][0], D[i-1][1]+cost[i][0])
+    D[i][1] = min(D[i-1][0]+cost[i][1], D[i-1][2]+cost[i][1])
+    D[i][2] = min(D[i-1][0]+cost[i][2], D[i-1][1]+cost[i][2])
+print(min(D[N-1]))
