748c7c999fa77dfcb5a0eb1db20f470a0cf695ed
imjaehyeong <dlawogud517@gmail.com>
2024-03-24 14:49:12 +0900

    11399_ATM 해결 완료

--- Baekjoon/12주차/11399_ATM.py
+++ Baekjoon/12주차/11399_ATM.py
@@ -1,24 +1,27 @@
 N = int(input())
-timeList = []
-timeList.extend(list(map(int,input().split())))
-print(timeList)
+timeList = list(map(int,input().split()))
+timeList.sort()
+totalSum=0
+for i in range(N):
+    totalSum += timeList[i]*(N-i)
+print(totalSum)
 # 빨리 끝내는 순서
 # 누적해서 더해나가야함
 
-length =len(timeList)
-totalsum=0
-def findMin(k):
-    global totalsum
-    if k == length:
-        print(totalsum)
-        return
-    minIndex= 0
-    minValue=100000
-    for i in range(len(timeList)): # 최솟값, 최소 인덱스 찾기
-        if minValue > timeList[i]:
-            minValue = timeList[i]
-            minIndex=i
-    print(timeList.pop(minIndex)) # timeList에서 최솟값 제거
-    totalsum+=minValue
-    findMin(k+1)
-findMin(0)
+# length =len(timeList)
+# totalsum=0
+# def findMin(k):
+#     global totalsum
+#     if k == length:
+#         print(totalsum)
+#         return
+#     minIndex= 0
+#     minValue=100000
+#     for i in range(len(timeList)): # 최솟값, 최소 인덱스 찾기
+#         if minValue > timeList[i]:
+#             minValue = timeList[i]
+#             minIndex=i
+#     print(timeList.pop(minIndex)) # timeList에서 최솟값 제거
+#     totalsum+=minValue
+#     findMin(k+1)
+# findMin(0)
