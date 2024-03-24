80254229bbcb5b8ffff5b435edc2e6c113cabf87
imjaehyeong <dlawogud517@gmail.com>
2024-03-24 11:33:47 +0900

    동전 0 풀이 완료

--- Baekjoon/12주차/11407_동전 0.py
+++ Baekjoon/12주차/11407_동전 0.py
@@ -1,25 +1,15 @@
-N, K = list(map(int, input().split()))
+N, K = map(int, input().split())
 valueList=[]
 for _ in range(N):
     valueList.append(int(input()))
 
-# 최소 거리값 찾기
-
 count = 0
-totalSum=0
-while True:
-    minlength=1000000000
-    idx=-1
-    # 최소거리값 찾기
-    for i in range(len(valueList)):
-        if totalSum+valueList[i] <= K :
-            if (4200-valueList[i]) < minlength:
-                minlength=4200-valueList[i]
-                idx= i # 최솟값 인덱스
-                # print(valueList[i])
+for i in reversed(range(N)):
+    if valueList[i] <= K :
+        count += K // valueList[i]
+        K = K % valueList[i]
+        if K==0:
+            break
 
-    totalSum+=valueList[idx]
-    count+=1
-    if totalSum == K:
-        print(count)
-        break
+print(count)
+
