a297bcab72b635f7f9413c4bd34bebf59dc5d373
imjaehyeong <dlawogud517@gmail.com>
2024-04-01 17:39:35 +0900

    문제 풀이 완료

--- Baekjoon/13주차/11055_가장큰증가하는부분수열.py
+++ Baekjoon/13주차/11055_가장큰증가하는부분수열.py
@@ -1,24 +1,39 @@
-N = int(input()) # O(n^2) 됨 n=1000
-numberList = list(map(int,input().split()))
-DP =[[0,0] for i in range(N)] # DP[i] i번째 까지의 수까지의 가장 큰 증가 부분수열
-#DP[i][0] : i번째 수 포함안함, DP[i][1] : i번째 수 포함함
-sequence = []
-DP[0][0]= 0
-DP[0][1] = numberList[0]
-sequence.append(numberList[0]) # DP[i][1] 즉 i번째 인덱스의 수 포함하는 경우 그 수열을 저장해둠
+# N = int(input()) # O(n^2) 됨 n=1000
+# numberList = list(map(int,input().split()))
+# DP =[[0,0] for i in range(N)] # DP[i] i번째 까지의 수까지의 가장 큰 증가 부분수열
+# #DP[i][0] : i번째 수 포함안함, DP[i][1] : i번째 수 포함함
+# sequence = []
+# DP[0][0]= 0
+# DP[0][1] = numberList[0]
+# sequence.append(numberList[0]) # DP[i][1] 즉 i번째 인덱스의 수 포함하는 경우 그 수열을 저장해둠
+#
+# if N>=2:
+#     for i in range(1,N):
+#         print(f"sequence : {sequence}")
+#         while sequence[-1] >= numberList[i]: #가장 위의 것이 크거나 같다고 하면 pop 작을떄까지 pop
+#             sequence.pop()
+#             if len(sequence)==0:
+#                 break
+#         sequence.append(numberList[i])
+#         DP[i][0] = max(DP[i-1][0], DP[i-1][1])
+#         DP[i][1] = sum(sequence)
+#         # print(f"포함 x : {DP[i][0]}")
+#         # print(f"포함 x : {DP[i][1]}")
+#
+# print(max(DP[N-1][0],DP[N-1][1]))
+#
 
-if N>=2:
-    for i in range(1,N):
-        # print(f"sequence : {sequence}")
-        while sequence[-1] >= numberList[i]: #가장 위의 것이 크거나 같다고 하면 pop 작을떄까지 pop
-            sequence.pop()
-            if len(sequence)==0:
-                break
-        sequence.append(numberList[i])
-        DP[i][0] = max(DP[i-1][0], DP[i-1][1])
-        DP[i][1] = sum(sequence)
-        # print(f"포함 x : {DP[i][0]}")
-        # print(f"포함 x : {DP[i][1]}")
+N = int(input())
+A = list(map(int, input().split()))
+d = [0] * N
 
-print(max(DP[N-1][0],DP[N-1][1]))
+d[0] = A[0]
 
+for i in range(N):
+    for j in range(i):
+        if A[i] > A[j]:
+            d[i] = max(d[i], d[j] + A[i])
+        else:
+            d[i] = max(d[i], A[i])
+
+print(max(d))
--- Baekjoon/13주차/1912_연속합.py
+++ Baekjoon/13주차/1912_연속합.py
@@ -1,18 +1,8 @@
-#1초 - 1억번 연산
-# n = 100000, n^2 = 백억 O (nlogn이나 n 필요)
-
 n = int(input())
-numberList = list(map(int,input().split()))
-print(numberList)
-DP = [0] * n # DP[i][j]  = 인덱스 i까지의 최대 연속 합, j는 마지막을 포함하는지 안하는 지 여부
-totalSum = numberList[0]
+array = list(map(int, input().split()))
 
-if n >=2:
-    for i in range(1, n): # O(n)
-        maxValueIncludeI=0
-        totalSum = max(totalSum, totalSum+numberList[i])
-        DP[i][0] = max(DP[i-1][1],DP[i-1][0])
-        DP[i][1] =
-# 10 -4 3
-
-
+d = [0] * n
+d[0] = array[0]
+for i in range(1, n):
+    d[i] = max(array[i], d[i-1]+array[i])
+print(max(d))
