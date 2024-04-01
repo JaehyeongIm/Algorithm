94320e1b251566a2b8c9517cbe726fd3e8a96685
imjaehyeong <dlawogud517@gmail.com>
2024-04-01 16:36:53 +0900

    11501_주식 해결

--- Baekjoon/13주차/11501_주식.py
+++ Baekjoon/13주차/11501_주식.py
@@ -1,2 +1,20 @@
 T = int(input())
+result = []
+for _ in range(T):
+    day = int(input())
+    stocks = list(map(int,input().split()))
+    stocks.reverse()
+    resultValue = 0
+    maxValue=stocks[0]
+    # 10 8 3 5
+    for i in range(day):
+        if maxValue>=stocks[i]:
+            resultValue += maxValue-stocks[i]
+        else:
+            maxValue=stocks[i]
 
+    result.append(resultValue)
+
+
+for i in result:
+    print(i)
